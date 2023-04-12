b84b1d86c1c2f36a56cf92428a2a06345d878270
Zweird-958 <xdfamilyytt@gmail.com>
2023-04-12 15:53:25 +0200

    Header

--- src/web/Page.js
+++ src/web/Page.js
@@ -5,21 +5,27 @@ const Page = (props) => {
   const { children } = props
 
   return (
-    <div className="flex flex-col bg-white shadow-md dark:bg-gray-900">
-      <div className="mx-auto flex w-full justify-between py-3 dark:text-white sm:w-2/3">
-        <HeaderLink href="/" className="font-bold">
-          NETMAPPER
-        </HeaderLink>
-        <div className="flex gap-6">
-          <HeaderLink href="/scan" className="font-medium">
-            SCAN
+    <div className="flex flex-col">
+      {/* Header Background */}
+      <div className="w-full bg-white py-3 shadow-md dark:bg-gray-900 dark:text-white">
+        {/* Header Texts */}
+        <div className="mx-auto flex w-full justify-between sm:w-2/3">
+          <HeaderLink href="/" className="font-bold">
+            NETMAPPER
           </HeaderLink>
-          <HeaderLink href="/historic" className="font-medium">
-            HISTORIQUE
-          </HeaderLink>
-          <Switch />
+          {/* Right texts */}
+          <div className="flex gap-6">
+            <HeaderLink href="/scan" className="font-medium">
+              SCAN
+            </HeaderLink>
+            <HeaderLink href="/history" className="font-medium">
+              HISTORIQUE
+            </HeaderLink>
+            <Switch />
+          </div>
         </div>
       </div>
+      {/* Body */}
       {children}
     </div>
   )
