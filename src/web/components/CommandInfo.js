7003b7e4adc30206fa7fa99a3f902739cc0a553f
Zweird-958 <xdfamilyytt@gmail.com>
2023-04-13 09:33:02 +0200

    Pretty Result

--- src/web/components/CommandInfo.js
+++ src/web/components/CommandInfo.js
@@ -1,9 +1,12 @@
+import PrettyResult from "@/web/components/PrettyResult"
+
 const CommandInfo = (props) => {
   const { label, result } = props
 
   return (
     <p>
-      <span className="font-bold">{label}</span> : {result}
+      <span className="text-lg font-bold">{label}</span> :{" "}
+      {label == "Résultat" ? <PrettyResult result={result} /> : result}
     </p>
   )
 }
