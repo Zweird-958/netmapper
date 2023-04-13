b3e72c1f4a8fdfcb99993d51b6f7229cddb2d2c7
Zweird-958 <xdfamilyytt@gmail.com>
2023-04-13 09:30:13 +0200

    Show result of scan

--- src/pages/scan.js
+++ src/pages/scan.js
@@ -1,8 +1,10 @@
 import Page from "@/web/Page"
 import Field from "@/web/components/Field"
 import Form from "@/web/components/Form"
+import HistorySummary from "@/web/components/HistorySummary"
 import SubmitButton from "@/web/components/SubmitButton"
 import api from "@/web/services/api"
+import { useState } from "react"
 import * as yup from "yup"
 // import child_process from "node:child_process"
 
@@ -24,25 +26,49 @@ const validationSchema = yup.object().shape({
 })
 
 const Scan = () => {
+  const [currentResult, setCurrentResult] = useState()
+
+  const resetResult = () => {
+    setCurrentResult()
+  }
+
   const handleSubmit = async (values) => {
-    await api.post("/nmap", values)
+    const {
+      data: { result },
+    } = await api.post("/nmap", values)
+    setCurrentResult(result)
   }
 
   return (
     <Page>
-      <div className="absolute -z-10 flex h-screen w-full items-center justify-center">
-        <Form
-          initialValues={initialValues}
-          validationSchema={validationSchema}
-          onSubmit={handleSubmit}
-        >
-          <p className="mx-auto w-fit text-3xl font-bold tracking-wider text-blue-400 transition-all hover:scale-110 dark:text-blue-600">
-            SCAN
-          </p>
-          <Field name="ip" placeholder="IP" />
-          <Field name="retries" placeholder="Max retries" type="number" />
-          <SubmitButton>SCAN</SubmitButton>
-        </Form>
+      <div className="absolute -z-10 flex h-screen w-full flex-col items-center justify-center">
+        {!currentResult ? (
+          <Form
+            initialValues={initialValues}
+            validationSchema={validationSchema}
+            onSubmit={handleSubmit}
+          >
+            <p className="mx-auto w-fit text-3xl font-bold tracking-wider text-blue-400 transition-all hover:scale-110 dark:text-blue-600">
+              SCAN
+            </p>
+            <Field name="ip" placeholder="IP" />
+            <Field name="retries" placeholder="Max retries" type="number" />
+            <SubmitButton>SCAN</SubmitButton>
+          </Form>
+        ) : (
+          <>
+            <HistorySummary
+              result={currentResult.result}
+              id={currentResult._id}
+            />
+            <button
+              onClick={resetResult}
+              className="rounded bg-sky-600 px-3 py-2 font-mono text-white transition-all duration-200 hover:scale-125"
+            >
+              Faire un nouveau scan
+            </button>
+          </>
+        )}
       </div>
     </Page>
   )
