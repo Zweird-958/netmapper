941383875370099a032c997efb56106f9e83aa3d
Zweird-958 <xdfamilyytt@gmail.com>
2023-04-13 16:28:57 +0200

    shadow

--- src/web/components/CommandDiv.js
+++ src/web/components/CommandDiv.js
@@ -6,7 +6,7 @@ const CommandDiv = (props) => {
   return (
     <div
       className={clsx(
-        "my-4 flex flex-col rounded border-2 border-sky-600 px-3 py-2 font-mono shadow-lg dark:text-white",
+        "dark:to-blue-transparent my-4 flex flex-col rounded border-2 border-sky-600 bg-gradient-to-r px-3 py-2 font-mono shadow-md shadow-blue-600 dark:from-transparent dark:text-white",
         className
       )}
       {...otherProps}
