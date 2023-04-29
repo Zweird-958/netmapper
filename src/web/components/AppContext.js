5abd8b867ab95356487cf86ac1af6e8a83be9ff6
Zweird-958 <xdfamilyytt@gmail.com>
2023-04-29 21:55:33 +0200

    fix: comment check if session is valid

--- src/web/components/AppContext.js
+++ src/web/components/AppContext.js
@@ -36,13 +36,13 @@ export const AppContextProvider = (props) => {
   useEffect(() => {
     ;(async () => {
       try {
-        const {
-          data: { error },
-        } = await api.get("/session")
+        // const {
+        //   data: { error },
+        // } = await api.get("/session")
 
-        if (error) {
-          return signOut()
-        }
+        // if (error) {
+        //   return signOut()
+        // }
 
         const jwt = localStorage.getItem(config.session.localStorageKey)
 
